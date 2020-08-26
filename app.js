06e0218cbc0478849ea50ba577ab503c2d7795fc
Troels Agergaard <TroelsAgergaard@users.noreply.github.com>
2020-08-26 09:35:08 +0200

    Remove notification

--- app.js
+++ app.js
@@ -12,38 +12,3 @@ if ('serviceWorker' in navigator) {
 		);
 	});
 }
-
-Notification.requestPermission(function(status) {
-	console.log('Notification permission status:', status);
-});
-
-function displayNotification() {
-	if (Notification.permission == 'granted') {
-		navigator.serviceWorker.getRegistration().then(function(reg) {
-			var options = {
-				body: 'Here is a notification body!',
-				icon: 'img/marioios.png',
-				vibrate: [ 100, 50, 100 ],
-				data: {
-					dateOfArrival: Date.now(),
-					primaryKey: 1
-				},
-				actions: [
-					{
-						action: 'explore',
-						title: 'Explore this new world',
-						icon: 'img/marioios.png'
-					},
-					{
-						action: 'close',
-						title: 'Close notification',
-						icon: 'img/marioios.png'
-					}
-				]
-			};
-			reg.showNotification('Hello World!', options);
-		});
-	}
-}
-
-displayNotification();
